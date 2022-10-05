a6b4454f53c65eaabbca2a4e3da799447afa7e39
Taisuke Fukuno <fukuno@jig.jp>
2022-10-05 10:03:00 +0900

    回遊できるメニューヘッダー追加
    https://github.com/code4fukui/fukui-kanko-stat/issues/14

---
+++ header.js
@@ -0,0 +1,31 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const insertFirst = (parent, c) => {
+  if (parent.children.length == 0) {
+    parent.appendChild(c);
+  } else {
+    parent.insertBefore(c, parent.children[0]);
+  }
+};
+
+const url = "./apps.csv";
+const smenu = await CSV.fetchJSON(url);
+console.log(smenu);
+
+const css = document.createElement("link");
+css.rel = "stylesheet";
+css.href = location.href.indexOf("[::]") >= 0 ? "style.css" : "https://code4fukui.github.io/fukui-kanko-stat/style.css";
+document.head.appendChild(css);
+
+const menu = document.createElement("ul");
+menu.className = "menu";
+for (const s of smenu) {
+  const li = document.createElement("li");
+  li.textContent = s.name;
+  li.onclick = () => location.href = s.url;
+  li.style.display = "inline-block";
+  li.style.listStyle = "none";
+  li.style.margin = ".2em 2em";
+  menu.appendChild(li);
+}
+insertFirst(document.body, menu);
