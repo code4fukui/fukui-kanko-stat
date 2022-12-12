d1346f6c1df92aa778908694f5aaea40dada7489
EiichiMiyagawa <m.monobyte@gmail.com>
2022-12-13 00:47:08 +0900

    天候情報に特化した画面を作成

--- weather.js
+++ weather.js
@@ -17,6 +17,7 @@ const weathers = {
 
 let weatherData;
 let wbgts;
+let nowDate;
 
 class Weather {
   constructor(parent) {
@@ -33,49 +34,60 @@ class Weather {
     const urlwbgt = "https://code4fukui.github.io/wbgt-japan/data/yohou_data.csv";
     wbgts = await CSV.toJSON(await CSV.fetch(urlwbgt));
     weatherData = {};
+    dayjs.locale("ja");
+    nowDate = dayjs();
     await Promise.all(Object.keys(weathers).map(async (key) => {
       weatherData[key] = await CSV.toJSON(await CSV.fetch(weathers[key]["url"]));
     }));
+
+    const onChangeAreaSelect = ((event) => {
+      this.createWeatherElement(event.target.value);
+    });
     
     const weatherGridElement = document.createElement("div");
     weatherGridElement.id = "weather-grid";
     parent.append(weatherGridElement);
     
-    const weekButtonElement = document.createElement("div");
-    weekButtonElement.id = "week-button";
-    
-    const titleElement = document.createElement("div");
-    titleElement.style = "font-weight: bold;";
-    titleElement.textContent = "気象情報";
-    weekButtonElement.append(titleElement);
-    
+    const weatherButtonElement = document.createElement("div");
+    weatherButtonElement.id = "weather-button";
+
+    const areaSelectElement = document.createElement("select");
+    areaSelectElement.onchange = onChangeAreaSelect
+    Object.keys(weathers).forEach((key) => {
+      const option = document.createElement("option");
+      option.textContent = key;
+      option.value = key;
+      areaSelectElement.append(option);
+    });
+    weatherButtonElement.append(areaSelectElement);
+
     const beforeWeekSpanElement = document.createElement("span");
     beforeWeekSpanElement.textContent = "前の週";
-    weekButtonElement.append(beforeWeekSpanElement);
+    weatherButtonElement.append(beforeWeekSpanElement);
     
     const beforeWeekButtonElement = document.createElement("input");
     beforeWeekButtonElement.type = "button";
     beforeWeekButtonElement.id = "before-week";
     beforeWeekButtonElement.value = "←";
-    weekButtonElement.append(beforeWeekButtonElement);
+    weatherButtonElement.append(beforeWeekButtonElement);
     
     const nextWeekButtonElement = document.createElement("input");
     nextWeekButtonElement.type = "button";
     nextWeekButtonElement.id = "next-week";
     nextWeekButtonElement.value = "→";
-    weekButtonElement.append(nextWeekButtonElement);
+    weatherButtonElement.append(nextWeekButtonElement);
     
     const nextWeekSpanElement = document.createElement("span");
     nextWeekSpanElement.textContent = "次の週";
-    weekButtonElement.append(nextWeekSpanElement);
+    weatherButtonElement.append(nextWeekSpanElement);
     
-    weatherGridElement.append(weekButtonElement);
+    weatherGridElement.append(weatherButtonElement);
     
     const weatherDataElement = document.createElement("div");
     weatherDataElement.id = "weather-data";
     weatherGridElement.append(weatherDataElement);
     
-    this.createWeatherElement();
+    this.createWeatherElement(areaSelectElement.value);
   }
   
   getWBGTMean(wbgt) {
@@ -116,66 +128,88 @@ class Weather {
     return { wbgt: max, hour: hour };
   }
   
-  async createWeatherElement() {
-    dayjs.locale("ja");
+  async createWeatherElement(area) {
     const baseDate = dayjs(weatherData[Object.keys(weathers)[0]][0]["年月日"]);
-    let nowDate = dayjs();
     
     const showWeather = (() => {
       const index = nowDate.diff(baseDate, "day");
       const showColumn = [
         "天気概況(昼：06時～18時)",
         "天気概況(夜：18時～翌日06時)",
+        "平均気温(℃)",
         "最高気温(℃)",
-        "最低気温(℃)"
+        "最高気温(℃)-時分",
+        "最低気温(℃)",
+        "最低気温(℃)-時分",
+        "日照時間(時間)",
+        "降水量の合計(mm)",
+        "平均湿度(％)",
+        "最小相対湿度(％)",
+        "最小相対湿度(％)-時分",
+        "平均風速(m/s)",
+        "最大風速(m/s)",
+        "最大風速(m/s)-時分",
+        "最大風速(m/s)-風向",
+        "最大瞬間風速(m/s)",
+        "最大瞬間風速(m/s)-時分",
+        "最大瞬間風速(m/s)-風向",
+        "最多風向(16方位)",
+        "合計全天日射量(MJ/㎡)",
+        "平均雲量(10分比)",
+        "降雪量合計(cm)",
+        "最深積雪(cm)",
+        "最深積雪(cm)-時分",
+        "平均蒸気圧(hPa)",
+        "平均現地気圧(hPa)",
+        "平均海面気圧(hPa)",
+        "最低海面気圧(hPa)",
+        "最低海面気圧(hPa)-時分"
       ];
       const weatherDataElement = document.getElementById("weather-data");
       weatherDataElement.innerHTML = "";
       
-      Object.keys(weathers).forEach((key) => {
-        const table = document.createElement("table");
-        
-        const headerTr = document.createElement("tr");
-        headerTr.append(document.createElement("th"));
+      const table = document.createElement("table");
+      
+      const headerTr = document.createElement("tr");
+      headerTr.append(document.createElement("th"));
+      for (let m = 0; m < 7; m++) {
+        const th = document.createElement("th");
+        th.textContent = `${nowDate.subtract(6 - m, "day").format("YYYY/MM/DD（ddd）")}`;
+        headerTr.append(th);
+      }
+      table.append(headerTr);
+      
+      for (let n = 0; n < showColumn.length; n++) {
+        const tr = document.createElement("tr");
+        const th = document.createElement("th");
+        th.textContent = showColumn[n];
+        tr.append(th);
         for (let m = 0; m < 7; m++) {
-          const th = document.createElement("th");
-          th.textContent = `${nowDate.subtract(6 - m, "day").format("YYYY/MM/DD（ddd）")}`;
-          headerTr.append(th);
-        }
-        table.append(headerTr);
-        
-        for (let n = 0; n < showColumn.length; n++) {
-          const tr = document.createElement("tr");
-          const th = document.createElement("th");
-          th.textContent = showColumn[n];
-          tr.append(th);
-          for (let m = 0; m < 7; m++) {
-            const td = document.createElement("td");
-            if (index - 6 + m < weatherData[key].length && index - 6 + m >= 0) {
-              td.textContent = weatherData[key][index - 6 + m][showColumn[n]];
-            }
-            tr.append(td);
-          }
-          table.append(tr);
-        }
-        
-        const wbgtTr = document.createElement("tr");
-        const wbgtTh = document.createElement("th");
-        wbgtTh.textContent = "暑さ指数";
-        wbgtTr.append(wbgtTh);
-        for (let m = 0; m < 7; m++) {
-          const maxWbgt = this.getMaxWBGT(weathers[key]["jmaStationNumber"], nowDate.subtract(6 - m, "day").format("YYYY/MM/DD"));
           const td = document.createElement("td");
-          td.textContent = this.getWBGTMean(maxWbgt["wbgt"])["desc"];
-          wbgtTr.append(td);
+          if (index - 6 + m < weatherData[area].length && index - 6 + m >= 0) {
+            td.textContent = weatherData[area][index - 6 + m][showColumn[n]];
+          }
+          tr.append(td);
         }
-        table.append(wbgtTr);
-        
-        const title = document.createElement("h3");
-        title.textContent = `${key}`;
-        weatherDataElement.append(title);
-        weatherDataElement.append(table);
-      });
+        table.append(tr);
+      }
+      
+      const wbgtTr = document.createElement("tr");
+      const wbgtTh = document.createElement("th");
+      wbgtTh.textContent = "暑さ指数";
+      wbgtTr.append(wbgtTh);
+      for (let m = 0; m < 7; m++) {
+        const maxWbgt = this.getMaxWBGT(weathers[area]["jmaStationNumber"], nowDate.subtract(6 - m, "day").format("YYYY/MM/DD"));
+        const td = document.createElement("td");
+        td.textContent = this.getWBGTMean(maxWbgt["wbgt"])["desc"];
+        wbgtTr.append(td);
+      }
+      table.append(wbgtTr);
+      
+      const title = document.createElement("h3");
+      title.textContent = `${area}`;
+      weatherDataElement.append(title);
+      weatherDataElement.append(table);
     });
     
     const onClickBeforeWeekButton = ((event) => {
