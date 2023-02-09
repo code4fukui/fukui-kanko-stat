404e99cf2237125638c1097633b1c30e5cdd4f77
EiichiMiyagawa <m.monobyte@gmail.com>
2023-02-10 00:48:50 +0900

    スマートフォンでのレイアウトを調整

--- ChartBar.js
+++ ChartBar.js
@@ -1,1 +1,2 @@
-export { ChartBar } from "https://code4fukui.github.io/chart-bar/chart-bar.js";
+// export { ChartBar } from "https://code4fukui.github.io/chart-bar/chart-bar.js";
+export { ChartBar } from "https://eiichimiyagawa.github.io/chart-bar-plotly/chart-bar.js";
