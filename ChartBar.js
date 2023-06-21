ea4d65ccbbad26d4f69132b569a059731a119eae
Taisuke Fukuno <fukuno@jig.jp>
2023-06-21 10:24:21 +0900

    fix chart

--- ChartBar.js
+++ ChartBar.js
@@ -1,2 +1,3 @@
-export { ChartBar } from "https://code4fukui.github.io/chart-bar/chart-bar.js";
+//export { ChartBar } from "https://code4fukui.github.io/chart-bar/chart-bar.js";
 //export { ChartBar } from "https://eiichimiyagawa.github.io/chart-bar-plotly/chart-bar.js";
+export { ChartBar } from "https://code4fukui.github.io/chart-bar-plotly/chart-bar.js";
