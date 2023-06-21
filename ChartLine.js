670fa8c539ccf4a3c88aebae87f9c4388b63161c
Taisuke Fukuno <fukuno@jig.jp>
2023-06-21 13:06:04 +0900

    fix ChartLine

--- ChartLine.js
+++ ChartLine.js
@@ -1,1 +1,3 @@
-export { ChartLine } from "https://code4fukui.github.io/chart-line/chart-line.js";
+//export { ChartLine } from "https://code4fukui.github.io/chart-line/chart-line.js";
+//import { ChartLine } from "https://eiichimiyagawa.github.io/chart-line-plotly/chart-line.js";
+export { ChartLine } from "https://code4fukui.github.io/chart-line-plotly/chart-line.js";
